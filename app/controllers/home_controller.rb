8292076ff065cb7743fae01f563a5b43595a69c4
henrikauy <auhenrik@gmail.com>
2023-10-11 16:02:49 +0800

    show quotes by user to be done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @quotes = Quote.includes(:source, :user).take(5)
+    @quotes = Quote.includes(:source).take(5)
   end
+
+  def uquotes
+    @quotes = Quote.includes(:source).where(user_id: session[:user_id])
+    end
 end
